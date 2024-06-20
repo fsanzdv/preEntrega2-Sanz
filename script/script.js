1df9bf60bffd119e877b4d9445014ad218b76540
Facundo <fsanzdv@gmail.com>
2024-06-20 23:50:03 +0200

    2da preentrega

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+
 function tasacion(){
    
     let año = prompt("Ingrese el año de su auto");
@@ -26,3 +27,12 @@ alert("El precio de su auto es: " + precio);
 
  tasacion();
  experiencia();
+
+
+
+ let año = [2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020];
+
+ for( let i = 0 ; i <= año.length ; i = i + 1){
+       
+        alert("Tipo de año vehiculo que se reciben: " + año[i])
+ }
