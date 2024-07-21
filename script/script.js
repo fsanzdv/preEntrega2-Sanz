c2b481419caad43aa458fbef9581500f81cadfe5
Facundo <fsanzdv@gmail.com>
2024-07-21 21:40:29 +0200

    arreglos 3er preentrega

--- script/script.js
+++ script/script.js
@@ -1,60 +1,70 @@
 
-/*function tasacion(){
-   
-    let año = prompt("Ingrese el año de su auto");
-año = parseInt(año);
 
-let precio;
+        function setData(){
+            
+            let nombreUsuario = document.getElementById("usuarioRegistro");
+            let contraseñaUsuario = document.getElementById("contraseñaRegistro");
 
-if (año >= 2000 && año <= 2010) {
-    precio = 1000;
-} else if (año > 2010 && año <= 2020) {
-    precio = 3000;
-} else {
-    precio = "No tenemos para ese auto";
-}
-alert("El precio de su auto es: " + precio);
+           let usuario = {nombre:nombreUsuario.value , contraseña:contraseñaUsuario.value};
+           console.log(usuario);
 
+           arregloUsuarios.push(usuario);
 
-}
- function experiencia() {
-    for( let i = 0 ; i <= 0 ; i = i + 1)
-        prompt("Desea que nos pongamos en contacto?")
-        prompt("Deje su telefono aqui")
-        alert("Gracias")
-   
- }
+           let arregloJson = JSON.stringify(arregloUsuarios);
 
- tasacion();
- experiencia();
-
-
-
- let año = [2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020];
-
- for( let i = 0 ; i <= año.length ; i = i + 1){
-       
-        alert("Tipo de año vehiculo que se reciben: " + año[i])
- }*/
-
-
-        function tasar() {
-            let anio = document.getElementById("anio");
-            console.log("El precio de su coche es: ", anio.value);
+           localStorage.setItem("arregloDeUsuarios", arregloJson);
         }
 
-        function respuesta(){
-            let anio = document.getElementById("anio");
-            let respuesta = document.getElementById("mensaje");
-            
-            
-            if (año >= 2000 && año <= 2010) {
-                precio = 1000;
-            } else if (año > 2010 && año <= 2020) {
-                precio = 3000;
-            } else {
-                precio = "No tenemos para ese auto";
+        function loginUsuarios() {
+            let nombreUsuario = document.getElementById("usuarioRegistro");
+            let contraseñaUsuario = document.getElementById("contraseñaRegistro");
+
+            let recuperandoDatos = localStorage.getItem("arregloDeUsuarios");
+            recuperandoDatos = JSON.parse(recuperandoDatos);
+            console.log(recuperandoDatos);
+
+            for(let usuario of recuperandoDatos ){
+                if(nombreUsuario.value == usuario.nombre &&  contraseñaUsuario.value == usuario.contraseña  ){
+                    window.location.href = 'http://127.0.0.1:5500/pages/tasacion.html';
+                    return;
+                }
+                else{
+                    document.body.innerHTML = `<h1>Usuario no registrado </h1>
+                    <a href="index.html">Volver</a>`;
+                }
+
             }
-            alert("El precio de su auto es: " + precio);
+        }   
 
-        }
+
+
+           let arregloUsuarios = [];
+
+           let btnRegistro = document.getElementById("btnRegistro");
+
+           btnRegistro.addEventListener("click", setData );
+           
+           let btnLogin = document.getElementById("btnLogin");
+
+           btnLogin.addEventListener("click", loginUsuarios);
+           
+           function tasar() {
+                let anio = document.getElementById("anio").value;
+                let respuesta = document.getElementById("respuesta");
+                let precio;
+            
+                anio = parseInt(anio); 
+            
+                if (anio >= 2000 && anio <= 2010) {
+                    precio = 1000;
+                } else if (anio > 2010 && anio <= 2020) {
+                    precio = 3000;
+                } else {
+                    precio = "No tenemos para ese auto";
+                }
+        
+
+            let parrafo  = document.createElement("p");
+            parrafo.innerText = "El precio de su auto es: " + precio;
+            respuesta.append(parrafo);
+            }
